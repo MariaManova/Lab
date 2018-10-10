f49ddfb7b0fc893cfdc2296a99e90667ca943c32
ManovaMaria <maffik1998@mail.ru>
2018-10-10 19:51:03 +0300

    lab01

---
+++ src/main/java/ru/wirbel/lab01/myapp/Application.java
@@ -0,0 +1,8 @@
+package ru.wirbel.lab01.myapp;
+
+public class Application {
+    public static void main(String[] args) {
+     System.out.println("Hello World");
+System.out.println("Привет");
+    }
+}
